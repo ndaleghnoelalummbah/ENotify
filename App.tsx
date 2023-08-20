be999d8886ad6ae2c782f0dce0071f3b08cbb892
ndaleghnoelalummbah <ndaleghnoela@gmail.com>
2023-08-20 19:00:07 +0100

    included some folders in .gitignore

--- App.tsx
+++ App.tsx
@@ -1,8 +1,20 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "./src/Screens/Home/Home";
+import Login from "./src/Screens/Login/Login";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
+    // <NavigationContainer>
+    //   <Stack.Navigator>
+    //     <Stack.Screen name="Home" component={Home} />
+    //     <Stack.Screen name="Login" component={Login} />
+    //   </Stack.Navigator>
+    // </NavigationContainer>
     <View style={styles.container}>
       <Text>I don't sem to get it !!</Text>
       <StatusBar style="auto" />
