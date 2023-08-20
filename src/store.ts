cf7f944f8e07aa8ba3c98d6d38b569da63aa0659
ndaleghnoelalummbah <ndaleghnoela@gmail.com>
2023-08-20 07:04:30 +0100

    setup environment

---
+++ src/store.ts
@@ -0,0 +1,13 @@
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+
+const rootReducer = combineReducers({
+  // liat reducers here
+})
+const store = configureStore({
+  reducer: rootReducer
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
